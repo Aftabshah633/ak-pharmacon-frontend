0a6814d09982d9838d2f7b9faa3b69e3587e8fab
pawan singh <pawant20singh@gmail.com>
2023-03-13 21:30:21 +0530

    added auth gaurd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    domain : 'http://akpharmacon:3000',
+    domain : 'http://akpharmacon.com:3000',
     localStorageToken : 'AKPHARMA',
-    whiteListDomains: ['localhost:3000','165.22.208.87:5000', '165.22.208.87:3000', '']
+    whiteListDomains: ['localhost:3000','165.22.208.87:5000', '165.22.208.87:3000', 'http://akpharmacon.com:3000']
 };
